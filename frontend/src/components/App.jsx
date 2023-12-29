47eea80da8bd0e8a0b65838a0a482f9288ee0dda
jkowalc5 <01169190@pw.edu.pl>
2023-12-29 18:00:57 +0100

    poprawka po merge

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CreateContestPage from "./CreateContestPage";
 import HomePage from "./HomePage";
-import CreateEntryPage from './CreateEntryPage';
+import CreateEntryPage from "./CreateEntryPage";
+import Entries from "./Entries";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/create-contest" element={<CreateContestPage />}/>
-        <Route path="/contest/:contestId" element={<CreateEntryPage />}/>
+        <Route path="/create-contest" element={<CreateContestPage />} />
+        <Route path="/contest/:contestId" element={<CreateEntryPage />} />
+        <Route path="/entries" element={<Entries />} />
       </Routes>
     </Router>
   );
