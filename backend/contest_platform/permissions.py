9f79b0ecd0850df3250fc97fd5ae323de45d8751
janekfilipecki <filipeckijan@gmail.com>
2024-01-02 12:36:07 +0100

    PK-62 changed permissions

--- backend/contest_platform/permissions.py
+++ backend/contest_platform/permissions.py
@@ -53,7 +53,7 @@ class ContestPermission(permissions.BasePermission):
 
         if view.action == "retrieve":
             return True
-        elif view.action in ["retrieve", "update", "partial_update", "destroy"]:
+        elif view.action in ["update", "partial_update", "destroy"]:
             return request.user.is_authenticated and request.user.is_staff
         else:
             return False
