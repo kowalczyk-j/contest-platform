1e79aa87fdec8782ff58b9dff8499f92febe0f2c
Filip <fbudzynski56@gmail.com>
2024-01-07 17:59:14 +0100

    PK-59 dodałem wyświetlanie dodania konkursów tylko dla uprzywilejowanych użytkowników

--- frontend/src/components/ContestListPage.jsx
+++ frontend/src/components/ContestListPage.jsx
@@ -15,6 +15,10 @@ import {
 import { styled } from "@mui/material/styles";
 import Logo from "../static/assets/Logo.png";
 import axios from "axios";
+import Navbar from "./Navbar.jsx";
+import TextButton from "./TextButton";
+
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 const GreenButton = styled(Button)({
   backgroundColor: "#95C21E",
@@ -28,17 +32,38 @@ const ContestIndexPage = () => {
   const [contests, setContests] = useState([]);
   const [selectedContest, setSelectedContest] = useState(null);
   const [isModalOpen, setModalOpen] = useState(false);
+  const [userData, setUserData] = useState({});
 
   useEffect(() => {
-    axios.get(`${import.meta.env.VITE_API_URL}api/contests/`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Token ' + sessionStorage.getItem("accessToken")
+    axios
+      .get(`${import.meta.env.VITE_API_URL}api/contests/`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Token " + sessionStorage.getItem("accessToken"),
+        },
+      })
+      .then((ret) => {
+        setContests(ret.data);
+      })
+      .catch((error) => console.error("Error:", error));
+
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}api/users/current_user/`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: "Token " + sessionStorage.getItem("accessToken"),
+            },
+          },
+        );
+        setUserData(response.data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
       }
-    }
-    ).then((ret) => {
-      setContests(ret.data)
-    }).catch(error => console.error("Error:", error));
+    };
+    fetchUserData();
   }, []);
 
   const handleContestClick = (contest) => {
@@ -52,15 +77,7 @@ const ContestIndexPage = () => {
 
   return (
     <div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <img style={{ width: "200px" }} src={Logo} alt="Logo" />
-      </div>
+      <Navbar></Navbar>
 
       <Typography
         variant="h4"
@@ -73,33 +90,7 @@ const ContestIndexPage = () => {
 
       <Grid container spacing={4} justifyContent="center">
         <Grid item>
-          <Card
-            style={{
-              width: "300px",
-              height: "400px",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <CardHeader title="Dodaj konkurs" />
-            <CardContent>
-              <Link to={"/create-contest"}>
-                <GreenButton>
-                  <div className="tile">
-                    <Typography align="center" style={{ color: "white" }}>
-                      Dodaj konkurs
-                    </Typography>
-                  </div>
-                </GreenButton>
-              </Link>
-            </CardContent>
-          </Card>
-        </Grid>
-
-        {contests.map((contest) => (
-          <Grid item key={contest.id}>
+          {userData.is_staff && (
             <Card
               style={{
                 width: "300px",
@@ -110,16 +101,88 @@ const ContestIndexPage = () => {
                 alignItems: "center",
               }}
             >
-              <CardHeader title={contest.title} />
+              <CardHeader title="Dodaj konkurs" />
               <CardContent>
-                <GreenButton onClick={() => handleContestClick(contest)}>
-                  <div className="contest-tile">
-                    <Typography align="center" style={{ color: "white" }}>
-                      Zobacz więcej
-                    </Typography>
-                  </div>
-                </GreenButton>
+                <Link to={"/create-contest"} style={{ textDecoration: "none" }}>
+                  <TextButton
+                    className="contest-title"
+                    style={{
+                      flex: 0.5,
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      fontSize: "1rem",
+                      color: "#95C21E",
+                    }}
+                  >
+                    Dodaj konkurs
+                  </TextButton>
+                </Link>
               </CardContent>
+            </Card>
+          )}
+        </Grid>
+
+        {contests.map((contest) => (
+          <Grid item key={contest.id}>
+            <Card
+              style={{
+                width: "300px",
+                height: "400px",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <div
+                style={{
+                  flex: 0.9,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <CardHeader title={contest.title} />
+                {/* jezeli jest zdjecie to nalezy je tu dodać */}
+                {contest.image && (
+                  <img
+                    src={contest.image}
+                    alt="Contest"
+                    style={{ width: "100%", maxHeight: "80%" }}
+                  />
+                )}
+              </div>
+
+              <div style={{ flex: 0.1, display: "flex", flexDirection: "row" }}>
+                <TextButton
+                  className="contest-title"
+                  onClick={() => handleContestClick(contest)}
+                  style={{
+                    flex: 0.5,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    fontSize: "1rem",
+                    color: "#95C21E",
+                  }}
+                >
+                  Zobacz więcej
+                </TextButton>
+
+                <TextButton
+                  style={{
+                    flex: 0.5,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    fontSize: "1rem",
+                    color: "#95C21E",
+                  }}
+                  endIcon={<ArrowForwardIcon />}
+                >
+                  Regulamin
+                </TextButton>
+              </div>
             </Card>
           </Grid>
         ))}
@@ -131,7 +194,9 @@ const ContestIndexPage = () => {
         fullWidth
         maxWidth="sm"
       >
-        <DialogTitle>{selectedContest?.title}</DialogTitle>
+        <DialogTitle style={{ fontSize: "1.8rem" }}>
+          {selectedContest?.title}
+        </DialogTitle>
         <DialogContent>
           <Typography variant="subtitle1" style={{ marginBottom: "10px" }}>
             Data rozpoczęcia: {selectedContest?.date_start}
@@ -139,9 +204,18 @@ const ContestIndexPage = () => {
           <Typography variant="subtitle1" style={{ marginBottom: "10px" }}>
             Data zakończenia: {selectedContest?.date_end}
           </Typography>
+
           <Typography variant="body1" style={{ marginBottom: "10px" }}>
             {selectedContest?.description}
           </Typography>
+
+          <TextButton
+            style={{ fontSize: "1rem", color: "#95C21E" }}
+            endIcon={<ArrowForwardIcon />}
+          >
+            Regulamin
+          </TextButton>
+
           {/* Add other details as needed */}
         </DialogContent>
         <DialogActions>
--- frontend/src/components/CreateContestPage.jsx
+++ frontend/src/components/CreateContestPage.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import 'reactjs-popup/dist/index.css'
-import Header from './Header';
-import BackButton from './BackButton';
-import ContestForm from './ContestForm';
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "reactjs-popup/dist/index.css";
+import Header from "./Header";
+import BackButton from "./BackButton";
+import ContestForm from "./ContestForm";
+import Navbar from "./Navbar";
 
 function CreateContestPage() {
   const navigate = useNavigate();
@@ -14,61 +14,70 @@ function CreateContestPage() {
     let criterion = formData.criterion;
     let contestId;
     let contestResponse, criterionResponse;
-  
+
     // Return the promise chain so that the calling function can await it
-    return axios.post(`${import.meta.env.VITE_API_URL}api/contests/`, formData, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Token ' + sessionStorage.getItem('accessToken')
-      },
-    })
-    .then((response) => {
-      if (response.status !== 201) {
-        throw new Error('Network response was not ok');
-      }
-      console.log(JSON.stringify(formData));
-      const result = response.data;
-      contestId = result.id;
-      console.log(result);
-      contestResponse = response;
-  
-      // Map criterion to promises and use Promise.all to wait for all to complete
-      const criterionPromises = criterion.map(c => {
-        c.contest = contestId;
-        return axios.post(`${import.meta.env.VITE_API_URL}api/assessment-criterion/`, c, {
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Token ' + sessionStorage.getItem('accessToken')
-          },
-        })
-        .then((response) => {
-          if (response.status !== 201) {
-            throw new Error('Network response was not ok');
-          }
-          console.log(JSON.stringify(c));
-          const result = response.data;
-          console.log(result);
-          return response; // Return the response for each criterion
+    return axios
+      .post(`${import.meta.env.VITE_API_URL}api/contests/`, formData, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Token " + sessionStorage.getItem("accessToken"),
+        },
+      })
+      .then((response) => {
+        if (response.status !== 201) {
+          throw new Error("Network response was not ok");
+        }
+        console.log(JSON.stringify(formData));
+        const result = response.data;
+        contestId = result.id;
+        console.log(result);
+        contestResponse = response;
+
+        // Map criterion to promises and use Promise.all to wait for all to complete
+        const criterionPromises = criterion.map((c) => {
+          c.contest = contestId;
+          return axios
+            .post(
+              `${import.meta.env.VITE_API_URL}api/assessment-criterion/`,
+              c,
+              {
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization:
+                    "Token " + sessionStorage.getItem("accessToken"),
+                },
+              },
+            )
+            .then((response) => {
+              if (response.status !== 201) {
+                throw new Error("Network response was not ok");
+              }
+              console.log(JSON.stringify(c));
+              const result = response.data;
+              console.log(result);
+              return response; // Return the response for each criterion
+            });
         });
+
+        // Wait for all criterion promises to resolve
+        return Promise.all(criterionPromises).then((responses) => {
+          criterionResponse = responses; // Store all criterion responses
+          return { contestResponse, criterionResponse }; // Return the final result object
+        });
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        throw error; // Rethrow the error to be caught by the calling function
       });
-  
-      // Wait for all criterion promises to resolve
-      return Promise.all(criterionPromises).then((responses) => {
-        criterionResponse = responses; // Store all criterion responses
-        return { contestResponse, criterionResponse }; // Return the final result object
-      });
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      throw error; // Rethrow the error to be caught by the calling function
-    });
   };
 
-  const handleBack = () => { navigate("/"); };
+  const handleBack = () => {
+    navigate("/");
+  };
 
   return (
     <div>
-      <Header />
+      <Navbar />
       <div className="main">
         <div className="back-btn">
           <BackButton clickHandler={handleBack} />
@@ -78,7 +87,7 @@ function CreateContestPage() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default CreateContestPage;
