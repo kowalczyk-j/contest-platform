f27a050fceb88b19530d6fa900a5db419541859e
Filip <fbudzynski56@gmail.com>
2024-01-09 18:40:45 +0100

    PK-59 zmiany backend potrzebne do wyświeltania imion i nazwisk uczestników konkursu w Entries

--- backend/contest_platform/serializers.py
+++ backend/contest_platform/serializers.py
@@ -4,38 +4,49 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
-        fields = ["id", "username", "first_name",
-                  "last_name", "email", "password", "is_staff",
-                  "is_superuser", "is_active", "date_joined"]
+        fields = [
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "password",
+            "is_staff",
+            "is_superuser",
+            "is_active",
+            "date_joined",
+        ]
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         user = User(
-            username=validated_data["username"], email=validated_data["email"])
+            username=validated_data["username"], email=validated_data["email"]
+        )
         user.set_password(validated_data["password"])
         user.save()
         return user
 
 
 class ContestSerializer(serializers.ModelSerializer):
-    date_start = serializers.DateField(input_formats=['%Y-%m-%d'])
-    date_end = serializers.DateField(input_formats=['%Y-%m-%d'])
+    date_start = serializers.DateField(input_formats=["%Y-%m-%d"])
+    date_end = serializers.DateField(input_formats=["%Y-%m-%d"])
 
     class Meta:
         model = Contest
-        fields = ("id",
-                  "title",
-                  "description",
-                  "date_start",
-                  "date_end",
-                  "individual",
-                  "type",
-                  "rules_pdf",
-                  "poster_img")
-        
+        fields = (
+            "id",
+            "title",
+            "description",
+            "date_start",
+            "date_end",
+            "individual",
+            "type",
+            "rules_pdf",
+            "poster_img",
+        )
+
 
 class PersonSerializer(serializers.ModelSerializer):
     class Meta:
@@ -44,31 +55,28 @@ class PersonSerializer(serializers.ModelSerializer):
 
 
 class EntrySerializer(serializers.ModelSerializer):
+    contestants = PersonSerializer(many=True, read_only=True)
+
     class Meta:
         model = Entry
-        fields = ("id",
-                  "contest",
-                  "contestants",
-                  "address",
-                  "email",
-                  "entry_title",
-                  "entry_file")
+        fields = (
+            "id",
+            "contest",
+            "contestants",
+            "address",
+            "email",
+            "entry_title",
+            "entry_file",
+        )
 
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
-        fields = ("id",
-                  "street",
-                  "number",
-                  "postal_code",
-                  "city")
+        fields = ("id", "street", "number", "postal_code", "city")
 
 
 class AssessmentCriterionSerializer(serializers.ModelSerializer):
     class Meta:
         model = AssessmentCriterion
-        fields = ("id",
-                  "contest",
-                  "description",
-                  "max_rating")
+        fields = ("id", "contest", "description", "max_rating")
--- backend/contest_platform/views.py
+++ backend/contest_platform/views.py
@@ -1,22 +1,32 @@
 from rest_framework import status
 from rest_framework.response import Response
 from .models import Address, AssessmentCriterion, Contest, Entry, User, Person
-from .serializers import (AddressSerializer, AssessmentCriterionSerializer,
-                          ContestSerializer, EntrySerializer, UserSerializer,
-                          PersonSerializer)
+from .serializers import (
+    AddressSerializer,
+    AssessmentCriterionSerializer,
+    ContestSerializer,
+    EntrySerializer,
+    UserSerializer,
+    PersonSerializer,
+)
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import api_view
 from rest_framework.authentication import TokenAuthentication
 from .permissions import UserPermission, ContestPermission, EntryPermission
 from rest_framework.decorators import action
-from django.contrib.auth import authenticate
 
 
-@api_view(["POST",])
+@api_view(
+    [
+        "POST",
+    ]
+)
 def logout(request):
     if request.method == "POST":
         request.user.auth_token.delete()
-        return Response({"message": "user has been logged out"}, status=status.HTTP_200_OK)
+        return Response(
+            {"message": "user has been logged out"}, status=status.HTTP_200_OK
+        )
 
 
 class ContestViewSet(ModelViewSet):
@@ -40,7 +50,7 @@ class EntryViewSet(ModelViewSet):
 
     def get_queryset(self):
         queryset = Entry.objects.all()
-        contest_id = self.request.query_params.get('contest', None)
+        contest_id = self.request.query_params.get("contest", None)
         if contest_id is not None:
             queryset = queryset.filter(contest=contest_id)
         return queryset
@@ -63,13 +73,12 @@ class AssessmentCriterionViewSet(ModelViewSet):
 
 
 class UserViewSet(ModelViewSet):
-
     serializer_class = UserSerializer
     queryset = User.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [UserPermission]
 
-    @action(detail=False, methods=['get'])
+    @action(detail=False, methods=["get"])
     def current_user(self, request):
         user = request.user
         serializer = self.get_serializer(user)
