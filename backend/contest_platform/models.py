fc1dd336b278d7770a83213171f3e89e1ea30308
kowalczyk-j <jakubkow505@gmail.com>
2024-05-21 19:51:23 +0200

    Contest states changing

--- backend/contest_platform/models.py
+++ backend/contest_platform/models.py
@@ -30,11 +30,15 @@ class Contest(models.Model):
     
     def save(self, *args, **kwargs):
         today = timezone.now().date()
-        if self.date_start == today:
-            self.status = 'ongoing'
-        elif self.date_end < today:
-            self.status = 'judging'
-        super().save(*args, **kwargs)
+        new_status = self.status
+        if self.date_start <= today < self.date_end and self.status != 'ongoing':
+            new_status = 'ongoing'
+        elif today >= self.date_end and self.status != 'judging':
+            new_status = 'judging'
+        
+        if new_status != self.status:
+            self.status = new_status
+            super().save(*args, **kwargs)
 
 # REQ_09A_END
 
