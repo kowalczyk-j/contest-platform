a050a30fb136fdb12903899c6e48c107f87a35c1
Paweł Dombrzalski <pawel.dombrzalski@gmail.com>
2024-05-25 01:29:50 +0200

    Commented this for now due to circular imports

--- backend/contest_platform/tasks.py
+++ backend/contest_platform/tasks.py
@@ -2,7 +2,7 @@ import dramatiq
 from django.core.mail import send_mass_mail
 from django.utils import timezone
 import datetime
-from .models import Contest
+# from .models import Contest
 
 
 @dramatiq.actor
@@ -10,13 +10,13 @@ def send_email_task(messages):
     send_mass_mail(messages, fail_silently=False)
 
 
-@dramatiq.actor
-def update_contest_status():
-    today = timezone.now().date()
-    
-    # Change status to 'ongoing' if date_start is today
-    Contest.objects.filter(date_start=today, status='not_started').update(status='ongoing')
-    
-    # Change status 'judging' if date_end was yesterday
-    yesterday = today - datetime.timedelta(days=1)
-    Contest.objects.filter(date_end=yesterday, status='ongoing').update(status='judging')
+# @dramatiq.actor
+# def update_contest_status():
+#     today = timezone.now().date()
+
+#     # Change status to 'ongoing' if date_start is today
+#     Contest.objects.filter(date_start=today, status='not_started').update(status='ongoing')
+
+#     # Change status 'judging' if date_end was yesterday
+#     yesterday = today - datetime.timedelta(days=1)
+#     Contest.objects.filter(date_end=yesterday, status='ongoing').update(status='judging')
