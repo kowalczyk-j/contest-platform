2b60d71715f725f137435e46e8b0c69a0c4efdf9
Piotras laptop <Piotr.kow.59@gmail.com>
2024-05-30 20:35:06 +0200

    test database setup

--- tests-lin.sh
+++ tests-lin.sh
@@ -3,18 +3,30 @@
 sudo apt-get update
 sudo apt-get -y install chromium-chromedriver
 
-sudo service postgresql start
+sudo service postgresql start &
 cd backend
 poetry install
 poetry shell
 
+sudo ../scripts/test_db_setup.sh
+
+export DJANGO_ENV=test
+
 python manage.py makemigrations
 python manage.py migrate
+
 python manage.py runserver &
 PID_DJANGO=$!
 
+sudo service rabbitmq-server start
+python manage.py rundramatiq &
+
+yacron -c yacron_jobs.yml &
+PID_YACRON=$!
+
 cd ../frontend
 npm install
+npm install --save @sentry/react
 npm run dev &
 PID_REACT=$!
 
